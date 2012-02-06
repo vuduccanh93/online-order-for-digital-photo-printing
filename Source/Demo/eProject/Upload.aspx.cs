d713405b21f05408ca47f534563508a8edb7a67d
hunglv_a04667@fpt.aptech.ac.vn <hunglv_a04667@fpt.aptech.ac.vn@7e7511bf-cdcb-ba12-42ce-e1ba4613e8ab>
2012-02-06 16:22:33 +0000

    Added Upload Image Code

---
+++ Source/Demo/eProject/Upload.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Cookies["username"].Value = "abc";
+        
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            // Get the HttpFileCollection
+            HttpFileCollection hfc = Request.Files;
+            string imgdir = Server.MapPath("Images");
+            System.IO.Directory.CreateDirectory(imgdir + "\\" + Request.Cookies["username"].Value);
+            imgdir = imgdir + "\\" + Request.Cookies["username"].Value;
+            for (int i = 0; i < hfc.Count; i++)
+            {
+                HttpPostedFile hpf = hfc[i];
+                if (hpf.ContentLength > 0)
+                {
+
+                    hpf.SaveAs(imgdir + "\\" + DateTime.Now.ToString("HHmmss_") +
+                      System.IO.Path.GetFileName(hpf.FileName));
+                    Response.Write("<b>File: </b>" + hpf.FileName + " <b>Size:</b> " +
+                        hpf.ContentLength + " <b>Type:</b> " + hpf.ContentType + " Uploaded Successfully <br/>");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+
+    }
+    
+}
